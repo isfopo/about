da64583b689266c5d7dccbb172245ec659a8b553
Isaac Poole <isaacpoolemusic@gmail.com>
2025-03-19 20:44:45 -0400

    ♻️ refactor(about-section): improve component readability
    - extract name and aboutMe strings into constants for better readability and maintainability

--- src/components/Section/AboutSection/index.tsx
+++ src/components/Section/AboutSection/index.tsx
@@ -2,21 +2,23 @@ import { Section } from "..";
 import { Frame } from "components/Frame";
 import styles from "./index.module.css";
 
-export const AboutSection = () => {
-  return (
-    <Section subject="about" className={styles["about"]}>
-      <Frame src={"public/photos/mountain_portrait.jpg"} />
-      <span>
-        <h2>Isaac Poole</h2>
-        <p>
-          Single-origin coffee bodega boys put a bird on it, edison bulb vice
+const name = "Isaac Poole";
+
+const aboutMe = `Single-origin coffee bodega boys put a bird on it, edison bulb vice
           man braid ethical grailed shaman tumeric retro af vaporware. Pour-over
           subway tile neutral milk hotel, dreamcatcher mukbang sartorial
           drinking vinegar. Stumptown man bun hammock everyday carry twee
           selvage gochujang 8-bit tilde craft beer. Poke gatekeep tumeric
           selvage pitchfork mumblecore. Helvetica locavore sus franzen shabby
-          chic big mood.
-        </p>
+          chic big mood.`;
+
+export const AboutSection = () => {
+  return (
+    <Section subject="about" className={styles["about"]}>
+      <Frame src={"public/photos/mountain_portrait.jpg"} />
+      <span>
+        <h2>{name}</h2>
+        <p>{aboutMe}</p>
       </span>
     </Section>
   );
