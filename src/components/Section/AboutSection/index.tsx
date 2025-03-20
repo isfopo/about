6743733a0eeeec25fcf545c6d46059af4f426e39
Isaac Poole <isaacpoolemusic@gmail.com>
2025-03-19 20:42:17 -0400

    ✨ feat(about-section): add detailed description to AboutSection
    - include a paragraph with engaging and descriptive text about Isaac Poole
    - enhance the content richness of the AboutSection component

--- src/components/Section/AboutSection/index.tsx
+++ src/components/Section/AboutSection/index.tsx
@@ -8,6 +8,15 @@ export const AboutSection = () => {
       <Frame src={"public/photos/mountain_portrait.jpg"} />
       <span>
         <h2>Isaac Poole</h2>
+        <p>
+          Single-origin coffee bodega boys put a bird on it, edison bulb vice
+          man braid ethical grailed shaman tumeric retro af vaporware. Pour-over
+          subway tile neutral milk hotel, dreamcatcher mukbang sartorial
+          drinking vinegar. Stumptown man bun hammock everyday carry twee
+          selvage gochujang 8-bit tilde craft beer. Poke gatekeep tumeric
+          selvage pitchfork mumblecore. Helvetica locavore sus franzen shabby
+          chic big mood.
+        </p>
       </span>
     </Section>
   );
