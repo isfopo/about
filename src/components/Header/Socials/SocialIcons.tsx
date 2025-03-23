1529c9e829979158f6ba63a7a2ba8febb6f9f073
Isaac Poole <isaacpoolemusic@gmail.com>
2025-03-23 19:46:22 -0400

    ♻️ refactor(assets): rename icons directory to logos
    - move bandcamp, github, and linkedin svg files from icons to logos
    - update import paths in SocialIcons.tsx to reflect new directory structure

--- src/components/Header/Socials/SocialIcons.tsx
+++ src/components/Header/Socials/SocialIcons.tsx
@@ -1,6 +1,6 @@
-import Github from "assets/icons/github.svg?react";
-import LinkedIn from "assets/icons/linkedin.svg?react";
-import Bandcamp from "assets/icons/bandcamp.svg?react";
+import Github from "assets/logos/github.svg?react";
+import LinkedIn from "assets/logos/linkedin.svg?react";
+import Bandcamp from "assets/logos/bandcamp.svg?react";
 
 export const SocialIcons: Record<string, React.ReactElement> = {
   github: <Github />,
