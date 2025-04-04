b482dd9ec64e25df85389817fe95c909a69f6c06
Isaac Poole <isaacpoolemusic@gmail.com>
2025-04-04 09:08:27 -0400

    💄 style(contactsection): update layout and styling
    - change flex direction to row and justify content to start
    - move padding and background color to icon class
    - add text class with column flex direction for label and value
    
    ✨ feat(contactsection): add labels for contact information
    
    - introduce labels for phone and email in contact section
    - display labels alongside corresponding contact values
    
    💄 style(skillssection): set fixed height for skills section
    
    - add height property to skills section for consistent layout
    
    💄 style(global): adjust global layout styles
    
    - remove flex display from body for improved flexibility
    - adjust section margin for better spacing across devices

--- src/components/Section/ContactSection/index.tsx
+++ src/components/Section/ContactSection/index.tsx
@@ -8,6 +8,11 @@ export interface Contact {
   mailto: `${string}@${string}.${string}`;
 }
 
+const labels: Record<keyof Contact, string> = {
+  tel: "Phone",
+  mailto: "Email",
+};
+
 export const ContactSection = (contact: Contact) => {
   return (
     <Section subject="contact" className={styles["contact"]}>
@@ -15,11 +20,16 @@ export const ContactSection = (contact: Contact) => {
       <div className={styles["container"]}>
         {Object.keys(contact).map((key) => {
           const value = contact[key as keyof Contact];
+          const label = labels[key as keyof Contact];
           const icon = ContactIcons[key as keyof Contact];
 
           return (
             <a key={key} href={`${key}:${contact[key as keyof Contact]}`}>
-              {icon} <span>{value}</span>
+              <div className={styles["icon"]}>{icon}</div>
+              <div className={styles["text"]}>
+                <p>{label}</p>
+                <p>{value}</p>
+              </div>
             </a>
           );
         })}
