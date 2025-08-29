83a14ee9e866d2d3df7d45066679c2496e493276
Isaac Poole <isaacpoolemusic@gmail.com>
2025-08-29 15:12:33 -0400

    feat: Add hero image and background
    Refactor Frame component to allow background. Adds radial and wave
    background svgs. Adds hero image to public folder.

--- src/components/Frame/index.tsx
+++ src/components/Frame/index.tsx
@@ -1,14 +1,18 @@
 import styles from "./index.module.css";
 
-export const Frame = (
-  props: React.DetailedHTMLProps<
+export interface FrameProps
+  extends React.DetailedHTMLProps<
     React.ImgHTMLAttributes<HTMLImageElement>,
     HTMLImageElement
-  >
-) => {
+  > {
+  background?: React.ReactElement;
+}
+
+export const Frame = (props: FrameProps) => {
   return (
     <div className={styles["frame"]}>
       <img {...props}></img>
+      <div className={styles["background"]}>{props.background}</div>
     </div>
   );
 };
--- src/components/Section/AboutSection/index.tsx
+++ src/components/Section/AboutSection/index.tsx
@@ -1,5 +1,7 @@
 import { Section } from "..";
 import { Frame } from "components/Frame";
+import ImageBackground from "assets/backgrounds/radiant_gradient.svg?react";
+import Background from "assets/backgrounds/wave_background.svg?react";
 import styles from "./index.module.css";
 
 export interface AboutSectionProps {
@@ -11,12 +13,13 @@ export interface AboutSectionProps {
 export const AboutSection = ({ name, tag, about }: AboutSectionProps) => {
   return (
     <Section subject="about" className={styles["about"]}>
-      <Frame src={"photos/mountain_portrait.jpg"} />
+      <Frame src={"photos/hero.webp"} background={<ImageBackground />} />
       <div className={styles["container"]}>
         <h1>{name}</h1>
         <h2>{tag}</h2>
         <p>{about}</p>
       </div>
+      <Background className={styles["background"]} />
     </Section>
   );
 };
